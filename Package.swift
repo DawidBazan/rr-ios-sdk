69590f35fdaafd5a389d707298f3a28ef683f739
Dawid <dawid@shape.dk>
2020-11-05 12:52:20 +0100

    Added cSettings

--- Package.swift
+++ Package.swift
@@ -20,6 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RichRelevanceSDK",
-            path: "Sources/RichRelevanceSDK"),
+            path: "Sources",
+            cSettings: [
+                .headerSearchPath("RichRelevanceSDK"),
+                .headerSearchPath("RichRelevanceSDK/Import"),
+                .headerSearchPath("RichRelevanceSDK/Import/Private"),
+                .headerSearchPath("RichRelevanceSDK/Private"),
+            ]),
     ]
 )
